0e584b6380b125e9af6ff45feb9152ae08217c82
Junyong Moon <jm8899@nyu.edu>
2025-03-03 15:17:29 +0400

    Lint pylint_demo.py

--- pylint_demo.py
+++ pylint_demo.py
@@ -1,10 +1,21 @@
-def sum(a, b):
+'''
+It has two simple functions sum_two_numbers and hello.
+Then it runs hello function twice.
+'''
+
+def sum_two_numbers(a, b):
+    '''
+    It takes two numbers and returns the sum of the two numbers
+    '''
     return a + b
 
 
 def hello(name):
+    '''
+    It prints "Hello, " and the passed name, concatenated
+    '''
     print(f"Hello, {name}!")
 
 
 hello("Alice")
-hello()
+hello("John")
